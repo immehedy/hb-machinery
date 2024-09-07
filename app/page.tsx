ecfcc94c116a12160f881c5c60f90c0659c2c2d4
Rubaith Adnan <dev.rubaith@gmail.com>
2024-09-08 04:56:25 +0600

    sanity data fetched successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 
-export default function Home() {
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+
+export default async function Home() {
+
+  //@ts-ignore
+  const products = await client.fetch(groq `*[_type=="product"]`);
+
+  console.log({products});
+
   return (
     <h1 className="underline">Hello World</h1>
   );
