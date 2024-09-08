688bfeb00b1241a7044d25412f7245349fc3268f
Mehedy Hassan <dev.mehedy.hassan@gmail.com>
2024-09-09 05:30:18 +0600

    build file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Products } from "./components/ui";
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      {/* @ts-expect-error Server Component */}
       <Products />
     </div>
   );
